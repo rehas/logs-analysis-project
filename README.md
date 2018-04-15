4309a55284792789f92bcbfd141fa6593de8a708
Berat Reha Sonmez <beratson@gmail.com>
2018-04-15 21:44:31 +0300

    Error percentage method addition, ReadMe update for view created

--- README.md
+++ README.md
@@ -24,8 +24,20 @@ I've created some extra views within this database:
   GROUP BY article_hits_view.path
   ORDER BY (count(*)) DESC;
 
+  log_types_count -- Shows daily grouped log types with counts and ratio of 404 responses to 200 responses. --
 
-logger.py has three functions
+  SELECT log.status,
+    log."time"::timestamp without time zone::date AS "time",
+    lag(count(*)) OVER (PARTITION BY (log."time"::timestamp without time zone::date)) AS lag,
+    count(*) AS cnt,
+    to_char(count(*)::numeric * 1.00 / lag(count(*)) OVER (PARTITION BY (log."time"::timestamp without time zone::date))::numeric, '9.999'::text) AS pct
+   FROM log
+  GROUP BY log.status, (log."time"::timestamp without time zone::date)
+  ORDER BY (log."time"::timestamp without time zone::date);
+
+
+
+logger.py has three functions called in order:
 
 1-) get_most_popular_articles => Returns most popular three articles
 
--- logger.py
+++ logger.py
@@ -21,13 +21,13 @@ def get_most_popular_articles():
     db = psycopg2.connect(DBPATH)
     c = db.cursor()
 
-# Creating query string parameter
-# Joining tables based on string replacement
-# Matching slug line with path
-# 
-# article_popularity is a view based on log table
-# Filters out the unsuccessful http attempts
-# Groups by path and adds a column named cnt for hit count to each path
+    # Creating query string parameter
+    # Joining tables based on string replacement
+    # Matching slug line with path
+    # 
+    # article_popularity is a view based on log table
+    # Filters out the unsuccessful http attempts
+    # Groups by path and adds a column named cnt for hit count to each path
     
     qry_get_most_popular_articles = \
      "select title, cnt \
@@ -39,9 +39,9 @@ def get_most_popular_articles():
     print("\n PRINTING MOST POPULAR ARTICLES \n")
     
 
-# Iterator is used to print only the first three most popular articles
-# This functionality can be embedded into the psql query but I thought
-# maybe in the future this function might be based on parameters. 
+    # Iterator is used to print only the first three most popular articles
+    # This functionality can be embedded into the psql query but I thought
+    # maybe in the future this function might be based on parameters. 
 
     iterator = 0
     for i in res:
@@ -65,11 +65,12 @@ def get_most_popular_authors():
     # order by sum desc;
 
     qry_get_most_popular_authors = \
-     "select author, name, sum(cnt) from \
-     articles, authors, article_popularity \
-     where articles.slug = replace(path, '/article/', '') \
-     and authors.id = articles.author \
-     group by author, name order by sum desc"
+     "select author, name, sum(cnt) \
+      from articles, authors, article_popularity \
+      where articles.slug = replace(path, '/article/', '') \
+      and authors.id = articles.author \
+      group by author, name order by sum desc"
+    
     c.execute(qry_get_most_popular_authors)
     res = c.fetchall()
 
@@ -85,3 +86,50 @@ def get_most_popular_authors():
     return res
 
 get_most_popular_authors()
+
+def get_error_days():
+    # Get all logs, based on date and type.
+    # 
+    # select status, time::timestamp::date, count(*) as cnt 
+    # from log group by (status, time::timestamp::date) order by time;
+
+    # include other status type count within the row including percentage
+    # 
+    # log_types_count view :
+    # 
+    # select status, time::timestamp::date, 
+    # lag(count(*)) over (partition by time::timestamp::date), 
+    # count(*) as cnt, 
+    # to_char(
+    #    count(*)*1.00 / 
+    #    lag(count(*)) over (partition by time::timestamp::date), 
+    #   '9.999') as pct 
+    # from log 
+    # group by (status, time::timestamp::date) 
+    # order by time;
+
+    db = psycopg2.connect(DBPATH)
+    c = db.cursor()
+
+    qry_get_error_days = \
+    "select time, lag + cnt as total, cnt as errors, \
+    to_char(cnt::float / (cnt + lag), '9.99') as P \
+    from log_types_count \
+    where (cnt::float / (cnt + lag) > 0.01::float) \
+    "
+
+    c.execute(qry_get_error_days)
+    res = c.fetchall()
+
+    print("\n PRINTING MOST ERROR DAYS \n")
+
+    iterator = 0
+    for i in res:
+        if iterator < 3:
+            print("Date: %s -- Total Hits: %s -- Errors: %s -- Percentage: %s"\
+             %( i[0],  i[1], i[2], i[3]))
+        iterator +=1
+    db.close()
+    return res
+
+get_error_days()
